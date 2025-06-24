c1abb33c5ca83f4edaadaa53069b010e43e60094
Hoang Le Duc <leduchoang011@gmail.com>
2025-06-23 20:52:24 -0400

    Add Vercel Analytics integration for enhanced tracking
    - Included `@vercel/analytics` package in both package.json and package-lock.json for improved analytics capabilities.
    - Integrated `Analytics` component in the layout for real-time user interaction tracking.
    - Updated dependencies to ensure compatibility with the new analytics tool.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Footer } from "@/components/ui/footer";
 import { Eye } from "@mynaui/icons-react";
 import { Navigation } from "@/components/navigation";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -54,6 +55,7 @@ export default function RootLayout({
             }}
           />
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
